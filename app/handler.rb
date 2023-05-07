ba71f01ffdae2cd6231b4b71849f632469269f80
Dimos Merachtsakis <dimosmera@gmail.com>
2023-05-08 00:31:57 +0800

    Add logic to evaluate buys in handler.rb

--- app/handler.rb
+++ app/handler.rb
@@ -1,12 +1,34 @@
 require 'json'
+require 'yaml'
 
 require_relative 'coingecko'
+
+# determines if we should sell or not based on the current price and profit margin given
+def evaluate_buys(buys, current_price, profit_margin)
+  buys.each do |buying_data|
+    buying_price = buying_data['price']
+
+    selling_price = buying_price * (1 + profit_margin)
+    puts selling_price
+
+    if current_price >= selling_price
+      puts 'SELL'
+    else
+      puts 'HOLD'
+    end
+  end
+end
 
 def crypto_profits(event:, context:)
   puts 'Calling Coingecko'
 
   coingecko_api = CoingeckoAPI.new
   puts coingecko_api.prices
+
+  flow_buys = YAML.load_file('data/flow.yml')
+  puts flow_buys
+
+  evaluate_buys(flow_buys, coingecko_api.prices['flow']['eur'], 0.1)
 
   puts "ENV['API_KEY'] = #{ENV['API_KEY']}"
 
