6ed8a93a6c13d1618b58e6642527a522a2bb4296
Dimos Merachtsakis <dimosmera@gmail.com>
2023-05-08 15:41:18 +0800

    Add SlackAPI, evaluate_buys and env.example.json files for post to Slack functionality and environment variable setup

--- app/handler.rb
+++ app/handler.rb
@@ -2,17 +2,20 @@ require 'json'
 require 'yaml'
 
 require_relative 'coingecko'
+require_relative 'slack'
 
 # determines if we should sell or not based on the current price and profit margin given
-def evaluate_buys(buys, current_price, profit_margin)
+def evaluate_buys(buys, current_price, profit_margin, slack_api)
   buys.each do |buying_data|
     buying_price = buying_data['price']
+    asset = buying_data['asset']
+    amount = buying_data['amount']
 
     selling_price = buying_price * (1 + profit_margin)
     puts selling_price
 
     if current_price >= selling_price
-      puts 'SELL'
+      slack_api.post_to_slack("#{amount} of #{asset} can now be sold for #{profit_margin * 100}% profit")
     else
       puts 'HOLD'
     end
@@ -25,12 +28,12 @@ def crypto_profits(event:, context:)
   coingecko_api = CoingeckoAPI.new
   puts coingecko_api.prices
 
+  slack_api = SlackAPI.new
+
   flow_buys = YAML.load_file('data/flow.yml')
   puts flow_buys
 
-  evaluate_buys(flow_buys, coingecko_api.prices['flow']['eur'], 0.1)
-
-  puts "ENV['API_KEY'] = #{ENV['API_KEY']}"
+  evaluate_buys(flow_buys, coingecko_api.prices['flow']['eur'], 0.1, slack_api)
 
   {
     statusCode: 200,
