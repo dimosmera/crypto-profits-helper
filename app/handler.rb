38d126c5456d198c0e1de344df85a20579a00433
Dimos Merachtsakis <dimosmera@gmail.com>
2023-05-09 12:42:16 +0800

    Add SOL and Update evaluate_buys to calculate and post profit percentage to Slack

--- app/handler.rb
+++ app/handler.rb
@@ -4,21 +4,20 @@ require 'yaml'
 require_relative 'coingecko'
 require_relative 'slack'
 
-# determines if we should sell or not based on the current price and profit margin given
+# Determines if we should sell or not based on the current price and profit margin given
 def evaluate_buys(buys, current_price, profit_margin, slack_api)
   buys.each do |buying_data|
     buying_price = buying_data['price']
+
+    selling_price = buying_price * (1 + profit_margin)
+
+    next if current_price < selling_price
+
     asset = buying_data['asset']
     amount = buying_data['amount']
 
-    selling_price = buying_price * (1 + profit_margin)
-    puts selling_price
-
-    if current_price >= selling_price
-      slack_api.post_to_slack("#{amount} of #{asset} can now be sold for #{profit_margin * 100}% profit")
-    else
-      puts 'HOLD'
-    end
+    profit_to_realize = ((current_price - buying_price) / buying_price) * 100
+    slack_api.post_to_slack("#{amount} of #{asset} can now be sold for #{profit_to_realize}% profit")
   end
 end
 
@@ -26,14 +25,16 @@ def crypto_profits(event:, context:)
   puts 'Calling Coingecko'
 
   coingecko_api = CoingeckoAPI.new
-  puts coingecko_api.prices
+  current_asset_prices = coingecko_api.prices
+  puts current_asset_prices
 
   slack_api = SlackAPI.new
 
   flow_buys = YAML.load_file('data/flow.yml')
-  puts flow_buys
+  sol_buys = YAML.load_file('data/sol.yml')
 
-  evaluate_buys(flow_buys, coingecko_api.prices['flow']['eur'], 0.1, slack_api)
+  evaluate_buys(flow_buys, current_asset_prices['flow']['eur'], 0.1, slack_api)
+  evaluate_buys(sol_buys, current_asset_prices['solana']['eur'], 0.2, slack_api)
 
   {
     statusCode: 200,
