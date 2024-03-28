db221efbdcf0891224375ec5d5ebb7a6b2f7dbf3
Dimos Merachtsakis <dimosmera@gmail.com>
2024-03-28 22:18:01 +0800

    accounts for custom profit margin and adds wen and bonk

--- app/handler.rb
+++ app/handler.rb
@@ -11,8 +11,9 @@ def evaluate_buys(buys, current_price, profit_margin, slack_api)
 
   buys.each do |buying_data|
     buying_price = buying_data['price']
+    profit_margin_to_use = buying_data['custom_profit_margin'] || profit_margin
 
-    selling_price = buying_price * (1 + profit_margin)
+    selling_price = buying_price * (1 + profit_margin_to_use)
 
     next if current_price < selling_price || buying_data['realized'] == true
 
@@ -77,6 +78,8 @@ def crypto_profits(event:, context:)
   algo_buys = YAML.load_file('data/algo.yml')
   doge_buys = YAML.load_file('data/doge.yml')
   jup_buys = YAML.load_file('data/jup.yml')
+  bonk_buys = YAML.load_file('data/bonk.yml')
+  wen_buys = YAML.load_file('data/wen.yml')
 
   evaluate_buys(flow_buys, current_asset_prices['flow']['eur'], 0.1, slack_api)
   evaluate_buys(link_buys, current_asset_prices['chainlink']['eur'], 0.1, slack_api)
@@ -84,17 +87,19 @@ def crypto_profits(event:, context:)
   evaluate_buys(near_buys, current_asset_prices['near']['eur'], 0.1, slack_api)
   evaluate_buys(algo_buys, current_asset_prices['algorand']['eur'], 0.1, slack_api)
 
-  evaluate_buys(jup_buys, current_asset_prices['jupiter-exchange-solana']['eur'], 2, slack_api)
+  evaluate_buys(jup_buys, current_asset_prices['jupiter-exchange-solana']['eur'], 0.35, slack_api)
+  evaluate_buys(bonk_buys, current_asset_prices['bonk']['eur'], 0.35, slack_api)
+  evaluate_buys(wen_buys, current_asset_prices['wen-4']['eur'], 0.35, slack_api)
 
   evaluate_buys(matic_buys, current_asset_prices['matic-network']['eur'], 0.15, slack_api)
 
   evaluate_buys(sol_buys, current_asset_prices['solana']['eur'], 0.4, slack_api)
   evaluate_buys(doge_buys, current_asset_prices['dogecoin']['eur'], 0.35, slack_api)
 
-  evaluate_buys(btc_buys, current_asset_prices['bitcoin']['eur'], 0.6, slack_api)
-  evaluate_buys(eth_buys, current_asset_prices['ethereum']['eur'], 0.8, slack_api)
+  evaluate_buys(btc_buys, current_asset_prices['bitcoin']['eur'], 0.5, slack_api)
+  evaluate_buys(eth_buys, current_asset_prices['ethereum']['eur'], 0.6, slack_api)
 
-  # evaluate_profits(jup_buys, '25/03/24', 'JUP', 'EUR', slack_api)
+  # evaluate_profits(doge_buys, '28/03/24', 'DOGE', 'EUR', slack_api)
 
   {
     statusCode: 200
